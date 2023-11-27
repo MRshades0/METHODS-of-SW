c5d63d8a24375f5e8baafb39ecf85ef11e8d5991
MRshades0 <149712499+MRshades0@users.noreply.github.com>
2023-11-27 12:50:19 -0600

    Add files via upload

--- menu.py
+++ menu.py
@@ -3,73 +3,80 @@ import sys
 import random
 from Methods_User_Class import User
 from cart_class import Cart
+from inventory import Inventory
 
-try:
-    connection = sqlite3.connect("methods.db")
+def main():
+    try:
+        connection = sqlite3.connect("methods.db")
 
-    print("Successful connection.")
+        print("Successful connection.")
 
-except:
-    print("Failed connection.")
+    except:
+        print("Failed connection.")
 
-    ## exits the program if unsuccessful
-    sys.exit()
+        ## exits the program if unsuccessful
+        sys.exit()
 
-cursor = connection.cursor()
-dataBaseName = "methods.db"
-user = User()
-cart = Cart()
-passed = False
-while(passed == False):
-    print("1. Login")
-    print("2. Create Account")
-    print("3. Logout")
-    print("\n")
-    select = input("Please select option from menu: ")
-    if (select == 1):
-        if(user.login() == False):
-            print("Invalid Login")
-        else:
+    user = User("methods.db", "USER", connection)
+    cart = Cart("methods.db", "CART", connection)
+    inventory = Inventory("methods.db", "INVENTORY", connection)
+    passed = False
+    while(passed == False):
+        print("1. Login")
+        print("2. Create Account")
+        print("3. Logout")
+        print("\n")
+        select = input("Please select option from menu: ")
+        if (select == 1):
+            if(user.login() == False):
+                print("Invalid Login")
+            else:
+                passed = True
+        elif(select == 2):
+            user.createAccount()
             passed = True
-    elif(select == 2):
-        user.createAccount()
-        passed = True
-    elif(select == 3):
-        user.logout()
-        passed = True
-    else:
-        print("Invalid menu selection, please try again\n")
-passed = False
-loggedIn = True
-while(loggedIn == True):
-    print("Menu:\n1. Logout\n2. View Account Information\n3. Inventory Information\n4. Cart Information")
-    select = input("Please select option from menu: ")
-    if(select == 2):
-        user.viewAccountInformation()
-    elif(select == 3):
-        inventorySelect = 0
-        while(inventorySelect != 1):
-            print("Inventory Menu:\n1. Go Back\n2. View Inventory\n3. Search Inventory")
-            inventorySelect = input("Select option from inventory menu: ")
-            if(inventorySelect == 2):
-                #view inventory
-            elif(inventorySelect == 3):
-                #search inventory
-            elif(inventorySelect != 1)
-                print("Invalid selection")
-    elif(select == 4):
-        cartSelect = 0
-        while(cartSelect != 1):
-            print("Cart Menu:\n1. Go Back\n2. View Cart\n3. Add Items to Cart\n4. Remove an Item from Cart\n5. Check Out")
-            cartSelect = input("Select option from cart menu: ")
-            if (cartSelect == 2):
-                userID = user.getUserID()
-                cart.viewCart(userID, #inventoryTable)
-            elif(cartSelect == 3)
-                #Add items to cart
-            elif(cartSelect == 4)
-                #Remove items from cart
-            elif(cartSelect == 5)
-                #checkout
-            elif(cartSelect != 1)
-                print("Invalid selection")
+        elif(select == 3):
+            user.logout()
+            passed = True
+        else:
+            print("Invalid menu selection, please try again\n")
+    passed = False
+    loggedIn = True
+    while(loggedIn == True):
+        print("Menu:\n1. Logout\n2. View Account Information\n3. Inventory Information\n4. Cart Information")
+        select = input("Please select option from menu: ")
+        if(select == 2):
+            user.viewAccountInformation()
+        elif(select == 3):
+            inventorySelect = 0
+            while(inventorySelect != 1):
+                print("Inventory Menu:\n1. Go Back\n2. View Inventory\n3. Search Inventory")
+                inventorySelect = input("Select option from inventory menu: ")
+                if(inventorySelect == 2):
+                    #view inventory
+                elif(inventorySelect == 3):
+                    #search inventory
+                elif(inventorySelect != 1):
+                    print("Invalid selection")
+        elif(select == 4):
+            cartSelect = 0
+            while(cartSelect != 1):
+                print("Cart Menu:\n1. Go Back\n2. View Cart\n3. Add Items to Cart\n4. Remove an Item from Cart\n5. Check Out")
+                cartSelect = input("Select option from cart menu: ")
+                if (cartSelect == 2):
+                    userID = user.getUserID()
+                    cart.viewCart(userID, 'inventory')
+                elif(cartSelect == 3):
+                    userID = user.getUserID()
+                    ISBN = input('ISBN of book you wish to add: ')
+                    cart.viewCart(userID, ISBN)
+                elif(cartSelect == 4):
+                    userID = user.getUserID()
+                    ISBN = input('ISBN of book you wish to remove: ')
+                    cart.viewCart(userID, ISBN)
+                elif(cartSelect == 5):
+                    userID = user.getUserID()
+                    cart.checkout(userID, inventory)
+                elif(cartSelect != 1):
+                    print("Invalid selection")
+main()
